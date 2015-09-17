5259513372deeb4e2a6b1d72c6324388e66b2884
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-17 18:10:20 +0100

    update letter spacing and styles on button press

--- config/api.js
+++ config/api.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://192.168.0.2:8080',
   leaderboardCountry: '/leaderboards/country',
   leaderboardUsers: '/leaderboards/users',
   leaderboardDistance: '/leaderboards/distance',
