d8424e5ada1c71870dc6c87365fa4851c73f4291
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-21 17:43:22 +0100

    added click events and assign body classes to reveal menu

--- src/js/views/menu-trigger.js
+++ src/js/views/menu-trigger.js
@@ -1,6 +1,23 @@
+import $          from 'jquery';
+import Backbone   from 'backbone';
 import Marionette from 'backbone.marionette';
 import menuTriggerTemplate from '../../templates/menu/trigger.hbs';
 
 export default Marionette.ItemView.extend({
   template: menuTriggerTemplate,
+  model: new Backbone.Model({ active: false }),
+  events: {
+    'click': 'onMenuTriggerClicked',
+  },
+  modelEvents: {
+    'change:active': 'onModelUpdate',
+  },
+
+  onMenuTriggerClicked: function() {
+    this.model.set('active', !this.model.get('active'));
+  },
+
+  onModelUpdate: function() {
+    $('body').toggleClass('menu-open', this.model.get('active'));
+  },
 });
