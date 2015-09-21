b7505442ab8de622372b324898bdab83007a7933
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-21 18:47:12 +0100

    added draw to menu

--- config/palette.js
+++ config/palette.js
@@ -1,8 +1,9 @@
 module.exports = {
-  'brand-first':  '#2b4451',  //dark blue
-  'brand-second': '#2f4b59',  //medium blue
-  'brand-third':  '#ffd7a3',  //kinda yellow
-  'brand-fourth': '#af8a65',  //brown
-  'brand-fifth':   '#a9e2ff',  //duck egg blue
-  'brand-sixth':  '#487389',  //text blue
+  'brand-first':     '#2b4451',  //dark blue
+  'brand-second':    '#2f4b59',  //medium blue
+  'brand-third':     '#ffd7a3',  //kinda yellow
+  'brand-fourth':    '#af8a65',  //brown
+  'brand-fifth':     '#a9e2ff',  //duck egg blue
+  'brand-sixth':     '#487389',  //text blue
+  'menu-background': '#e3b781',
 };
