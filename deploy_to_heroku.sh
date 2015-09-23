8a8b7e7882f6ae2db5e5e44027e28f53e1cb38d4
Le Roux Bodenstein <leroux@gitter.im>
2015-09-23 12:59:55 +0100

    Remove dead end

--- deploy_to_heroku.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git checkout -b deploy-`date +"%F-%H_%M_%S"`
-npm install
-npm run build
-rm -rf .gitignore
-git add -A
-git commit -m 'deploy'
-git push heroku master -f
-git checkout master
