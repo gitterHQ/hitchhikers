e869326ea618d307bd03b38d16d4c1ba930c352c
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-24 17:15:32 +0100

    remove navigate to loading on submit login

--- src/js/routers/index-router.js
+++ src/js/routers/index-router.js
@@ -47,7 +47,7 @@ export default Backbone.Router.extend({
 
             this.listenTo(indexLayout, 'login:clicked', () => {
               this.stopListening(indexLayout, 'login:clicked');
-              this.navigate('loading', { trigger: true });
+              this.onRouteLoading();
             });
 
             indexLayout.render();
