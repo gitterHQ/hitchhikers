f899f5cdb30bd4ffa3bd998b0a0a77962b0c60e6
Le Roux Bodenstein <leroux@gitter.im>
2015-09-24 12:42:36 +0100

    Require secret on /private api.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser')
 var authuser = require('./lib/middleware/authuser');
+var privatesecret = require('./lib/middleware/privatesecret');
 var cors = require('cors');
 
 var app = express();
@@ -20,12 +21,12 @@ app.use(cors());
 app.use('/github', require('./lib/routes/github'));
 app.use('/user', authuser, require('./lib/routes/user'));
 app.use('/users', require('./lib/routes/users'));
-app.use('/private', require('./lib/routes/private'));
+app.use('/private', privatesecret, require('./lib/routes/private'));
 app.use('/leaderboards', require('./lib/routes/leaderboards'));
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  
+
   if (err.status == 301) {
     return res.redirect(err.location);
   }
